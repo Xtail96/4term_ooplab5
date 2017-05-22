d417e7ffc2a6cd0320920069e7c3ce235310e52f
Xtail <xtail1996@yandex.ru>
2017-05-22 14:33:56 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+#include "methodslab5.h"
+
+using namespace stepik;
+
+void printVector(vector< shared_ptr<Shape> > &container, std::string name)
+{
+    std::cout << "Vector #" + name + ":" << std::endl;
+    for(auto it = container.begin(); it != container.end(); it++)
+    {
+        (*it)->print(std::cout);
+    }
+    std::cout << std::endl << "___________" << std::endl;
+
+}
+
+int main(/*int argc, char *argv[]*/)
+{
+    system("clear");
+    vector< shared_ptr<Shape> > v1 = createRandomShapeVector(5);
+    vector< shared_ptr<Shape> > v2 = createRandomShapeVector(5);
+
+    Ellipse e(0, 0, 5, 10, 0, 1, Color(165, 90, 50));
+    Round r(0, 0, 10);
+    Arc a(0, 0, -M_PI/2, M_PI/2, 10);
+    Round r2(0, 0, 10, 0, 1, Color(220, 0, 0));
+
+//    vector< shared_ptr<Shape> > v1;
+//    v1.push_back(shared_ptr<Shape>(&e));
+//    v1.push_back(shared_ptr<Shape>(&r));
+//    v1.push_back(shared_ptr<Shape>(&a));
+
+
+//    vector< shared_ptr<Shape> > v2;
+//    v2.push_back(shared_ptr<Shape>(&e));
+//    v2.push_back(shared_ptr<Shape>(&r2));
+//    v2.push_back(shared_ptr<Shape>(&a));
+//    v2.push_back(shared_ptr<Shape>(&r));
+
+
+
+    std::cout << std::endl << "Origin Vectors:" << std::endl;
+
+    printVector(v1, "v1");
+    printVector(v2, "v2");
+
+    std::cout << std::endl << "___________" << std::endl << std::endl;
+
+
+    std::cout << std::endl << "Unmodify Algorithm:" << std::endl;
+    bool isSplitted = checkVector(v1, redis);
+    if(isSplitted)
+    {
+        std::cout << "Container v1 is split" << std::endl;
+    }
+    else
+    {
+        std::cout << "Container v1 is not split" << std::endl;
+    }
+
+    std::cout << std::endl << "Unmodify Algorithm:" << std::endl;
+    isSplitted = checkVector(v2, redis);
+    if(isSplitted)
+    {
+        std::cout << "Container v2 is split" << std::endl;
+    }
+    else
+    {
+        std::cout << "Container v2 is not split" << std::endl;
+    }
+
+    std::cout << std::endl << "Modify Algorithm:" << std::endl;
+    //printVector(v1, "v1");
+    //printVector(v2, "v2");
+    //std::cout << std::endl << "___________" << std::endl << std::endl;
+
+    vector< shared_ptr<Shape> > difference = makeSortedDifference(v1, v2);
+    std::cout << "Difference size = " << difference.size() << std::endl << std::endl;
+    printVector(difference, "Sorted difference between v1 and v2");
+
+    return 0;
+}
---
+++ methodslab5.h
@@ -0,0 +1,113 @@
+#ifndef METHODSLAB5_H
+#define METHODSLAB5_H
+
+#include "shape.h"
+#include "vectorstepik.h"
+#include "sharedpointer.h"
+
+// создаем вектор случайных фигур с размером size
+stepik::vector< stepik::shared_ptr<Shape> > createRandomShapeVector(unsigned int size)
+{
+    stepik::vector< stepik::shared_ptr<Shape> > v;
+    for(unsigned int i = 0; i < size; i++)
+    {
+        Shape* tmp = Shape::createRandomShape();
+        v.push_back(stepik::shared_ptr<Shape>(tmp));
+    }
+    return v;
+}
+
+// функция-предикат для немодифицирующего алгоритма
+bool redis(Shape *s)
+{
+    Color shapeColor = s->getColor();
+    if(shapeColor.red >= 100 && shapeColor.green < 100 && shapeColor.blue < 100)
+    {
+       return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+// 1) Проверка разделен ли диапазон на две группы по заданному критерию.
+bool checkVector(stepik::vector< stepik::shared_ptr<Shape> > &container,
+                std::function<bool (Shape*)> pred)
+{
+    if(container.size() > 1)
+    {
+        bool isStateChange = false;
+        auto firstElement = container.begin();
+        bool firstState = pred(firstElement->get());
+
+        for(auto it = container.begin(); it != container.end();  it++)
+        {
+            if(!isStateChange)
+            {
+                if(pred(it->get()) != firstState)
+                {
+                    isStateChange = true;
+                }
+            }
+            else
+            {
+                if(pred(it->get()) == firstState)
+                {
+                    isStateChange = false;
+                    break;
+                }
+            }
+        }
+        return isStateChange;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool cmp(const stepik::shared_ptr<Shape> &first, const stepik::shared_ptr<Shape> &second)
+{
+    return (first.get()->getId() < second.get()->getId());
+}
+
+void dividedifferentElelments(const stepik::vector<stepik::shared_ptr<Shape> > &first,
+                         const stepik::vector<stepik::shared_ptr<Shape> > &second,
+                         stepik::vector<stepik::shared_ptr<Shape> > &difference);
+// 2) Создать упорядоченную разность двух диапазонов.
+stepik::vector< stepik::shared_ptr<Shape> > makeSortedDifference(stepik::vector< stepik::shared_ptr<Shape> > &first,
+                stepik::vector< stepik::shared_ptr<Shape> > &second)
+{
+    stepik::vector< stepik::shared_ptr<Shape> >difference;
+    dividedifferentElelments(first, second, difference);
+    dividedifferentElelments(second, first, difference);
+
+    std::sort(difference.begin(), difference.end(), cmp);
+
+    return difference;
+}
+
+void dividedifferentElelments(const stepik::vector< stepik::shared_ptr<Shape> > &first,
+                              const stepik::vector< stepik::shared_ptr<Shape> > &second,
+                              stepik::vector< stepik::shared_ptr<Shape> > &difference)
+{
+    for(auto it1 : first)
+    {
+        bool isFinded = false;
+        for(auto it2 : second)
+        {
+            if(it1.get() == it2.get())
+            {
+                isFinded = true;
+                break;
+            }
+        }
+        if(!isFinded)
+        {
+            difference.push_back(it1);
+        }
+    }
+}
+
+#endif // METHODSLAB5_H
